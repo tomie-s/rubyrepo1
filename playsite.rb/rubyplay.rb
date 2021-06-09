45c9d2324cf1bddce124d0ed2f5445415ef23e30
Tomi E. Salami <salami.tomie@gmail.com>
2021-06-08 22:19:48 -0400

    commits for edits to file

--- playsite.rb/rubyplay.rb
+++ playsite.rb/rubyplay.rb
@@ -1,3 +1,10 @@
 puts "What is your first name?"
 first_name = gets.chomp
 puts "Thank you, you said your first name is #{first_name}"
+#*** 'gets' will get you a string and '.chomp' will make string start on new line
+
+#*** gets.chomp usually returns a string.
+#**to use it for a number, the string has to be converted to an integer using .to_i
+puts "Enter a number to multiply by 2"
+input = gets.chomp
+puts input.to_i * 2
